1872b339a7155ed5797b18443ce994eb357e7cbd
Gianluca <marcon.gluca@gmail.com>
2019-02-08 15:14:43 +0100

    Cleaned emacs directory

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,13 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("99c86852decaeb0c6f51ce8bd46e4906a4f28ab4c5b201bdc3fdf85b24f88518" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "980f0adf3421c25edf7b789a046d542e3b45d001735c87057bccb7a411712d09" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(org-agenda-files
-   (quote
-    ("~/dotfiles/emacs/.emacs.d/configuration.org" "~/Dropbox/org/work.org" "~/Nextcloud/PhD/org/work.org")))
- '(package-selected-packages
-   (quote
-    (elpy helm-ag deadgrep google-this yasnippet-snippets which-key use-package spacemacs-theme solarized-theme snazzy-theme smart-mode-line-powerline-theme rainbow-mode rainbow-delimiters ox-twbs org-ref org-evil org-bullets org-ac oceanic-theme markdown-preview-mode magit lorem-ipsum lab-themes helm-org-rifle fzf evil-org evil-matchit evil-leader evil-goggles evil-embrace evil-commentary emojify company beacon auto-complete-auctex auctex atom-dark-theme))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
