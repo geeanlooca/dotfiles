5996c0ab2cb455140b8e2b9a94c42c60140db0a8
Gianluca <marcon.gluca@gmail.com>
2019-03-10 02:08:25 +0100

    Useless cleanup

--- vim/prepare.sh
+++ vim/prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
- git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
- vim +PluginInstall +qall
+vim +PluginInstall +qall
