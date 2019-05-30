cc9ead581a588f7b0a0858de8e3a79f7d0a3ce3e
Gianluca Marcon <marcon.gluca@gmail.com>
2019-05-30 11:45:05 +0200

    changes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,7 +6,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(package-selected-packages
+   (quote
+    (lorem-ipsum beacon google-this smart-mode-line elpy yasnippet-snippets yasnippet auctex-latexmk ac-math auctex helm-ag deadgrep evil-magit magit which-key fzf helm-org-rifle ox-twbs org-ref evil-org org-evil org-bullets org-ac evil-commentary evil-surround evil-leader evil lab-themes use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- profile/.profile
+++ profile/.profile
@@ -1,10 +1,11 @@
-export QT_QPA_PLATFORMTHEME="qt5ct"
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 export SURFRAW_browser=firefox
 export EDITOR=vim
-export QT_LOGGING_RULES="qt5ct.debug=false"
-
-export GREEN=/media/green
-export RED1=/media/red1
-export RED2=/media/red2
+# export QT_LOGGING_RULES="qt5ct.debug=false"
+# 
+# export GREEN=/media/green
+# export RED1=/media/red1
+# export RED2=/media/red2
 export MATLABPATH=/home/gianluca/.config/matlab
+export PATH=$PATH:$HOME/bin
+export CHEAT_COLORS=true
